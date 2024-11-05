90b77006f60473d47878ec895181e7fc40ce8f86
Matías <matias.agustin.sosa.04@gmail.com>
2024-11-05 11:02:29 -0300

    mati

--- Olimpo de los Datos/app.py
+++ Olimpo de los Datos/app.py
@@ -5,20 +5,21 @@ from PIL import Image
 import plotly.graph_objects as go
 import random
 
+# Importamos los datasets
 df = pd.read_csv("Data.app\df_PBI.csv")
 df2 = pd.read_csv("Data.app\df_poblacion.csv")
 
-
+# Icono de la página 
 img = Image.open("Imagenes\olimpo.png")
 
-
+# Título de la página 
 st.set_page_config(page_title = "Olimpo de los Datos", page_icon = img)
 
-
+# Navegador de páginas 
 menu = ["Inicio", "Predición Población", "Predioción PBI", "Analisís Deportivo JJOO"]
-elecion = st.sidebar.selectbox("# **Navegación**", menu)
+elecion = st.sidebar.selectbox("Navegación", menu)
 
-
+# Personalización de colores
 st.markdown("""
     <style>
     .stApp {
@@ -81,7 +82,7 @@ def predic_uno(pais, anos_a_predecir, df, columna):
         anos_existentes = sorted(data['Year'].unique())
 
         # Crear un DataFrame con los años a predecir (solo los que no existen)
-        anos_a_predecir_unicos = [ano for ano in range(anos_existentes[0], anos_existentes[-1]+anos_a_predecir+1)]# if ano not in anos_existentes
+        anos_a_predecir_unicos = [ano for ano in range(anos_existentes[0], anos_existentes[-1]+anos_a_predecir+1)]
         future = pd.DataFrame({'ds': anos_a_predecir_unicos})
 
         # Realizar la predicción
@@ -134,7 +135,7 @@ def predic_multiple(pais, anos_a_predecir, df, columna):
     anos_existentes = sorted(data['Year'].unique())
 
     # Crear un DataFrame con los años a predecir (solo los que no existen)
-    anos_a_predecir_unicos = [ano for ano in range(anos_existentes[0], anos_existentes[-1]+anos_a_predecir+1)]# if ano not in anos_existentes
+    anos_a_predecir_unicos = [ano for ano in range(anos_existentes[0], anos_existentes[-1]+anos_a_predecir+1)]
     future = pd.DataFrame({'ds': anos_a_predecir_unicos})
 
     # Realizar la predicción
@@ -144,28 +145,33 @@ def predic_multiple(pais, anos_a_predecir, df, columna):
     forecast = forecast[['ds', 'yhat']]
     forecast['ds'] = forecast['ds'].dt.year
 
+    # Retorno la predicción 
     return forecast
 
 
 def multiples(año_multiple, paises, data, columna_P):
 
     try:
-            
+        # Creó una figura y guardo los colores usados
         fig = go.Figure()
         colores_usados = []
-            
+
+        # Recorro los países y hago sus predicciones 
         for i in paises:
             predic = predic_multiple( i, año_multiple, data, columna_P)
                 
             color_aleatorio = None
-                
+            # seleccionar colores random    
             while color_aleatorio is None or color_aleatorio in colores_usados:
                 color_aleatorio = f'rgb({random.randint(0, 255)}, {random.randint(0, 255)}, {random.randint(0, 255)})'
 
+            # Guardo los colores usados 
             colores_usados.append(color_aleatorio)
-
+            
+            # Agrego la linea de la predicción 
             fig.add_trace(go.Scatter(x=predic['ds'], y=predic['yhat'],name=f"Predicción {i}",line=dict(color=color_aleatorio)))
         
+        # Gráfico final
         fig.update_layout(xaxis_title="Años", yaxis_title = columna_P, plot_bgcolor='#F2E8C9')
         fig.update_layout(
         xaxis=dict(
@@ -185,6 +191,7 @@ def multiples(año_multiple, paises, data, columna_P):
 
 
 
+# Página 1
 if elecion == "Predición Población":
 
     st.markdown("# <center><font color='#F2AB27'>Predicciones Población</font></center>", unsafe_allow_html=True)
@@ -217,7 +224,7 @@ if elecion == "Predición Población":
     multiples(año_multiple_2, opcion_multiple_2, df2, "Population")
 
 
-
+# Página 2
 elif elecion == "Predioción PBI":
 
     st.markdown("# <center><font color='#F2AB27'>Predicciones PBI</font></center>", unsafe_allow_html=True)
@@ -251,13 +258,13 @@ elif elecion == "Predioción PBI":
     multiples(año_multiple, opcion_multiple, df, "PBI")
 
 
-
+# Página 3
 elif elecion == "Analisís Deportivo JJOO":
     st.markdown("# <center><font color='#F2AB27'>Analisís Deportivo JJOO</font></center>", unsafe_allow_html=True)
 
- 
+    # Dashboard
     st.components.v1.iframe(
-        src="https://app.powerbi.com/view?r=eyJrIjoiNzI0MjkzNGQtMDgyMS00OTliLWEyYTYtMzM3YmJmNTQ2NzMxIiwidCI6IjcxOWU4ZTRkLWZkZDMtNDQxZC05NDcyLTM0MDAxNGJiMTM1NyIsImMiOjR9",  # Reemplaza con tu URL de inserción
+        src="https://app.powerbi.com/view?r=eyJrIjoiNzI0MjkzNGQtMDgyMS00OTliLWEyYTYtMzM3YmJmNTQ2NzMxIiwidCI6IjcxOWU4ZTRkLWZkZDMtNDQxZC05NDcyLTM0MDAxNGJiMTM1NyIsImMiOjR9",
         width=800,  # Ancho del iframe
         height=500,  # Alto del iframe
         scrolling=True  # Habilitar el scrolling si el contenido es grande
@@ -266,12 +273,13 @@ elif elecion == "Analisís Deportivo JJOO":
 
 
 
-
+# Página de inicio
 else:
     st.markdown("# <center><font color='#F2AB27'>Olimpo de los Datos</font></center>", unsafe_allow_html=True)
     
     st.markdown("## <center><font color='#F2E8C9'>¡Bienvenido Usuario!</font></center>", unsafe_allow_html=True)
     
+    # Centrar la imagen
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.image("Imagenes\Robot.png", width = 300)
@@ -283,6 +291,7 @@ else:
     
     st.markdown(""" #### <center><font color='#F2E8C9'> **¡Pero eso no es todo!** Si eres un apasionado del deporte, nuestra sección de Predicciones Olímpicas te permitirá sumergirte en el emocionante mundo de las competencias internacionales. Con nuestros modelos predictivos, podrás anticipar los próximos campeones y las tendencias que marcarán el futuro de los Juegos Olímpicos.</font></center>""", unsafe_allow_html=True)
 
+    # Centrar la imagen
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.image("Imagenes\olimpo.png", width = 300)
